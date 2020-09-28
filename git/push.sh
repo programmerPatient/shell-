40845f4f33b4de6d49196a3b81fb8a690f08bed5
programmerPatient <494429797@qq.com>
2020-09-28 18:02:45 +0800

    git push脚本更新

--- git/push.sh
+++ git/push.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-logPath="../log/$(date "+%Y")/$(date "+%m")/$(date "+%Y.%m.%d").txt"
+logPath="../log/$(date "+%Y.%m.%d").txt"
 echo "-------------------------$(date "+%H:%M:%S")-----------------------------------" >> ${logPath}
 echo "执行的命令：" >> ${logPath}
 git add -A
 if [ $? -eq 0 ]; then
-    echo "git add -A" >> ${logPath}
+    echo -e "\e[1;42mgit add -A \e[0m" >> ${logPath}
     git commit -m "$1"
     if [ $? -eq 0 ]; then
-        echo "git commit -m \"$1\"" >> ${logPath}
+        echo -e "\e[1;42mgit commit -m \"$1\"\e[0m" >> ${logPath}
         git push
         if [ $? -eq 0 ]; then
-            echo "git push" >> ${logPath}
+            echo "\e[1;42mgit push \e[0m" >> ${logPath}
         else
-            echo "error:$?" >> ${logPath}
+            echo "\e[31;42merror:$? \e[0m" >> ${logPath}
         fi
     else
-        echo "error:$?" >> ${logPath}
+        echo "\e[31;42merror:$? \e[0m" >> ${logPath}
     fi
 else
- echo "error:$?" >> ${logPath}
+ echo "\e[31;42merror:$? \e[0m" >> ${logPath}
 fi
 echo "--------------------------end---------------------------------------" >> ${logPath}
