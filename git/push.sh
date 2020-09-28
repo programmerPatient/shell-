6c17924058ad2506b290093a3ea55a31226c8280
programmerPatient <494429797@qq.com>
2020-09-28 17:22:01 +0800

    git push脚本更新

--- git/push.sh
+++ git/push.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 echo "git push start"
+echo $(date "+%Y.%m.%d")
+logPath="../log/$(date "+%Y.%m.%d").txt"
 git add -A
 if [ $? -eq 0 ]; then
-    echo "git add success" > ../log/$(date "+%Y.%m.%d")/$(date).txt
+    echo "git add success" > ${logPath}
     git commit -m "$1"
     if [ $? -eq 0 ]; then
-        echo "git commit success"
+        echo "git commit success" > ${logPath}
         git push
         if [ $? -eq 0 ]; then
-            echo "git push success"
+            echo "git push success" > ${logPath}
         else
-            echo "$?"
+            echo $? > ${logPath}
         fi
     else
-        echo "$?"
+        echo $? > ${logPath}
     fi
 else
-    echo "$?"
+    echo $? > ${logPath}
 fi
