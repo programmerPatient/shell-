0632f04cbecf43805d290b241dbb42f229647a9d
programmerPatient <494429797@qq.com>
2020-09-28 18:20:13 +0800

    git push脚本更新

--- git/push.sh
+++ git/push.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 logPath="$(dirname $(pwd))/log/$(date "+%Y-%m-%d").txt"
-echo "-------------------------$(date "+%H:%M:%S")-----------------------------------" >> ${logPath}
+echo "-------------------------$(date "+%H:%M:%S")master分支直接提交-----------------------------------" >> ${logPath}
 echo "执行的命令：" >> ${logPath}
 git add -A
-if [ $? -eq 0 ]; then
-    echo "git add -A" >> ${logPath}
-    git commit -m "$1"
-    if [ $? -eq 0 ]; then
-        echo "git commit -m \"$1\"" >> ${logPath}
-        git push
-        if [ $? -eq 0 ]; then
-            echo "git push" >> ${logPath}
-        else
-            echo "error:$?" >> ${logPath}
-        fi
-    else
-        echo "error:$?" >> ${logPath}
-    fi
+      if [ $? -eq 0 ]; then
+          echo "git add -A" >> ${logPath}
+          git commit -m "$1"
+          if [ $? -eq 0 ]; then
+              echo "git commit -m \"$1\"" >> ${logPath}
+              git push
+              if [ $? -eq 0 ]; then
+                  echo "git push" >> ${logPath}
+              else
+                  echo "error:$?" >> ${logPath}
+              fi
+          else
+              echo "error:$?" >> ${logPath}
+          fi
 else
  echo "error:$?" >> ${logPath}
 fi
-echo "--------------------------end---------------------------------------" >> ${logPath}
+echo "--------------------------end------------------------------------------------------" >> ${logPath}
