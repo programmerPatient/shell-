b5c2d8454a5904b17c69d4d5d0e9f34dcc92d5c6
programmerPatient <494429797@qq.com>
2020-09-28 18:10:57 +0800

    git push脚本更新

--- git/push.sh
+++ git/push.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-logPath="../log/$(date "+%Y.%m.%d").txt"
-echo -e "\e[1;42mGreed Background\e[0m" >> ${logPath}
+logDirPath="../log/$(date "+%Y")/$(date "+%m")"
+if [ ! -d logDirPath ];then
+  mkdir logDirPath
+fi
+logPath="${logDirPath}/$(date "+%Y-%m-%d")"
 echo "-------------------------$(date "+%H:%M:%S")-----------------------------------" >> ${logPath}
 echo "执行的命令：" >> ${logPath}
 git add -A
 if [ $? -eq 0 ]; then
-    echo -e "\e[1;42mgit add -A \e[0m" >> ${logPath}
+    echo "git add -A" >> ${logPath}
     git commit -m "$1"
     if [ $? -eq 0 ]; then
-        echo -e "\e[1;42mgit commit -m \"$1\"\e[0m" >> ${logPath}
+        echo "git commit -m \"$1\"" >> ${logPath}
         git push
         if [ $? -eq 0 ]; then
-            echo "\e[1;42mgit push \e[0m" >> ${logPath}
+            echo "git push" >> ${logPath}
         else
-            echo "\e[31;42merror:$? \e[0m" >> ${logPath}
+            echo "error:$?" >> ${logPath}
         fi
     else
-        echo "\e[31;42merror:$? \e[0m" >> ${logPath}
+        echo "error:$?" >> ${logPath}
     fi
 else
- echo "\e[31;42merror:$? \e[0m" >> ${logPath}
+ echo "error:$?" >> ${logPath}
 fi
 echo "--------------------------end---------------------------------------" >> ${logPath}
