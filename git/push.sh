c265c62f75db662e980095caa9556ea447b46544
programmerPatient <494429797@qq.com>
2020-09-28 17:29:17 +0800

    git push脚本更新

--- git/push.sh
+++ git/push.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 logPath="../log/$(date "+%Y.%m.%d").txt"
-echo "git push start" > ${logPath}
+sed "git push start" > ${logPath}
 git add -A
 if [ $? -eq 0 ]; then
     git commit -m "$1"
     if [ $? -eq 0 ]; then
         git push
         if [ $? -ne 0 ]; then
-            echo $? > ${logPath}
+            sed $? > ${logPath}
         fi
     else
-        echo $? > ${logPath}
+        sed $? > ${logPath}
     fi
 else
-    echo $? > ${logPath}
+    sed $? > ${logPath}
 fi
-echo "git push end" > ${logPath}
+sed "git push end" > ${logPath}
