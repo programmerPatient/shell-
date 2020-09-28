ba7c7c894d201dc31e557fb138c6e28839ddf8f5
programmerPatient <494429797@qq.com>
2020-09-28 17:57:12 +0800

    git push脚本更新

--- git/push.sh
+++ git/push.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 logPath="../log/$(date "+%Y.%m.%d").txt"
 echo "-------------------------$(date "+%H:%M:%S")-----------------------------------" >> ${logPath}
-echo "git push start" >> ${logPath}
 echo "执行的命令：" >> ${logPath}
 git add -A
 if [ $? -eq 0 ]; then
     echo "git add -A" >> ${logPath}
     git commit -m "$1"
     if [ $? -eq 0 ]; then
-        echo "git commit -m $1" >> ${logPath}
+        echo "git commit -m \"$1\"" >> ${logPath}
         git push
         if [ $? -eq 0 ]; then
             echo "git push" >> ${logPath}
@@ -21,5 +20,4 @@ if [ $? -eq 0 ]; then
 else
  echo "error:$?" >> ${logPath}
 fi
-echo "git push end" >> ${logPath}
 echo "--------------------------end---------------------------------------" >> ${logPath}
