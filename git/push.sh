260ceb5e4c49e681eb85a6792872fa6fc2ae6db8
programmerPatient <494429797@qq.com>
2020-09-29 14:39:05 +0800

    ssssss

--- git/push.sh
+++ git/push.sh
@@ -3,13 +3,13 @@ logPath="$(dirname $(pwd))/log/$(date "+%Y-%m-%d").txt"
 echo "-------------------------master分支直接提交($(date "+%H:%M:%S"))-----------------------------------" >> ${logPath}
 echo "执行的命令：" >> ${logPath}
 echo "git add -A" >> ${logPath}
-git add -A >> ${logPath}  2>&1
+git add -A 2>> ${logPath}
 if [ $? -eq 0 ]; then
   echo "git commit -m \"$1\"" >> ${logPath}
-  git commit -m "$1" >> ${logPath}  2>&1
+  git commit -m "$1" 2>> ${logPath}
   if [ $? -eq 0 ]; then
       echo "git push" >> ${logPath}
-      git push 2>> ${logPath}  
+      git push 2>> ${logPath}
       if [ $? -ne 0 ]; then
          echo "\033[31m git push错误 \033[0m"
          echo "git push错误" >> ${logPath}
