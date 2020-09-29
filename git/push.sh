4421b089af426d2e8d52e82206beea0c75942681
programmerPatient <494429797@qq.com>
2020-09-29 14:42:56 +0800

    ssssss

--- git/push.sh
+++ git/push.sh
@@ -5,14 +5,18 @@ echo "执行的命令：" >> ${logPath}
 echo "git add -A" >> ${logPath}
 git add -A 2>> ${logPath}
 if [ $? -eq 0 ]; then
-  echo "git commit -m \"$1\"" >> ${logPath}
+  echo "git add -A 结束" >> ${logPath}
+  echo "git commit -m \"$1\" 开始" >> ${logPath}
   git commit -m "$1" 2>> ${logPath}
   if [ $? -eq 0 ]; then
-      echo "git push" >> ${logPath}
+      echo "git commit -m \"$1\" 结束" >> ${logPath}
+      echo "git push 开始" >> ${logPath}
       git push 2>> ${logPath}
       if [ $? -ne 0 ]; then
          echo "\033[31m git push错误 \033[0m"
          echo "git push错误" >> ${logPath}
+      else
+         echo "git push 结束" >> ${logPath}
       fi
   else
       echo "\033[31m git commit -m \"$1\"错误 \033[0m"
