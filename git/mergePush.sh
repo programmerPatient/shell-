d08bbe5aae7824a57cadb4a1342220edf80ad8e7
programmerPatient <494429797@qq.com>
2020-09-29 15:53:46 +0800

    marry分支合并提交2

--- git/mergePush.sh
+++ git/mergePush.sh
@@ -15,38 +15,40 @@ if [ $? -eq 0 ]; then
                     if [ $? -eq 0 ]; then
                         git checkout marry 2>>${logPath}
                         if [ $? -eq 0 ]; then
-                            echo "提交成功 成功切回marry分支"
+                            git merge master 2>>${logPath}
+                            if [ $? -eq 0 ]; then
+                                echo "切回marry分支并合并master分支代码成功"
+                            else
+                                echo -e "\033[31m error : git merge master 切回marry分支并合并master分支代码失败 \033[0m"
+                                echo "error : git merge master 切回marry分支并合并master分支代码失败" >> ${logPath}
+                            fi
                         else
-                            echo "error : git check marry  切回marry分支失败"
-                            echo "error : git check marry  切回marry分支失败" >> ${logPath}
+                            echo -e "\033[31m error : master分支下 git check marry  切回marry分支失败 \033[0m"
+                            echo "error : master分支下 git check marry  切回marry分支失败" >> ${logPath}
                         fi
                     else
-                      echo "error : git push  合并marry分支后的代码提交失败"
-                      echo "error : git push  合并marry分支后的代码提交失败" >> ${logPath}
+                      echo -e "\033[31m error : master分支下 git push  合并marry分支后的代码提交失败 \033[0m"
+                      echo "error : master分支下 git push  合并marry分支后的代码提交失败" >> ${logPath}
                     fi
                 else
-                   echo "error : git merge marry 合并分支失败"
-                   echo "error : git merge marry 合并分支失败" >> ${logPath}
+                   echo -e "\033[31m error : master分支下 git merge marry 合并分支失败 \033[0m"
+                   echo "error : master分支下 git merge marry 合并分支失败" >> ${logPath}
                 fi
             else
-                echo "error : git pull 拉取代码失败"
-                echo "error : git pull 拉取代码失败" >> ${logPath}
+                echo -e "\033[31m error : master分支下 git pull 拉取代码失败 \033[0m"
+                echo "error : master分支下 git pull 拉取代码失败" >> ${logPath}
             fi
         else
-            echo "error : git checkout master 切换分支失败"
+            echo -e "\033[31m error : git checkout master 切换分支失败 \033[0m"
             echo "error : git checkout master 切换分支失败" >> ${logPath}
         fi
     else
-         echo "error : git commit -m \"$1\""
+         echo -e "\033[31m error : git commit -m \"$1\" 失败 \033[0m"
          echo "error : git commit -m \"$1\" 失败" >> ${logPath}
     fi
 else
     echo "error : git add ."
     echo "error : git add .  提交新文件(new)和被修改(modified)文件，不包括被删除(deleted)文件 失败" >> ${logPath}
-<<<<<<< HEAD
- fi
-=======
 fi
->>>>>>> marry
 echo "--------------------------end---------------------------------------------------------" >> ${logPath}
 
