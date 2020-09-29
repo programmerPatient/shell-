56d6fa06855990a4395d607f94dc00f2606eda2c
programmerPatient <494429797@qq.com>
2020-09-29 15:24:21 +0800

    合并marry分支测试

--- git/mergePush.sh
+++ git/mergePush.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+echo "-------------------------master分支直接提交($(date "+%H:%M:%S"))-----------------------------------" >> ${logPath}
 logPath="$(dirname $(pwd))/log/$(date "+%Y-%m-%d").log"
 git add . 2>> ${logPath}
 if [ $? -eq 0 ]; then
     git commit -m "$1" 2>>${logPath}
     if [ $? -eq 0 ]; then
         git checkout master 2>> ${logPath}
-        if [$? -eq 0 ]; then
+        if [ $? -eq 0 ]; then
             git pull 2>> ${logPath}
-            if [$? -eq 0 ]; then
+            if [ $? -eq 0 ]; then
                 git merge marry 2>> ${logPath}
-                if [$? -eq 0 ]; then
+                if [ $? -eq 0 ]; then
                     git push 2>> ${logPath}
                     if [ $? -eq 0 ]; then
                         git checkout marry 2>>${logPath}
@@ -42,5 +43,6 @@ if [ $? -eq 0 ]; then
 else
     echo "error : git add ."
     echo "error : git add .  提交新文件(new)和被修改(modified)文件，不包括被删除(deleted)文件 失败" >> ${logPath}
- fi
+fi
+echo "--------------------------end---------------------------------------------------------" >> ${logPath}
 
