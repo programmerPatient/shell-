9c290025ad9eff483a08e1992544466bfb73fbb0
programmerPatient <494429797@qq.com>
2020-09-29 14:33:01 +0800

    ssssss

--- git/push.sh
+++ git/push.sh
@@ -10,6 +10,16 @@ if [ $? -eq 0 ]; then
   if [ $? -eq 0 ]; then
       echo "git push" >> ${logPath}
       git push >> ${logPath}  2>&1
+      if [ $? -ne 0 ]; then
+         echo "\033[31m git push错误 \033[0m"
+         echo "git push错误" >> ${logPath}
+      fi
+  else
+      echo "\033[31m git commit -m \"$1\"错误 \033[0m"
+      echo git commit -m \"$1\"错误 >> ${logPath}
   fi
+else
+   echo "\033[31m git add错误 \033[0m"
+   echo git add错误 >> ${logPath}
 fi
 echo "--------------------------end---------------------------------------------------------" >> ${logPath}
